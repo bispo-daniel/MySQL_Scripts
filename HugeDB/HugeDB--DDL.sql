33a5a62ce9fb006177d0043f9b356d5d137f1acb
bispo-daniel <dbispo.work@gmail.com>
2023-04-12 19:37:58 -0300

    updateHugeDB

--- HugeDB/HugeDB--DDL.sql
+++ HugeDB/HugeDB--DDL.sql
@@ -1,17 +1,15 @@
 create database hugeDB;
-
 use hugeDB;
 
 create table person (
-	id int(9) not null auto_increment,
+	id int unsigned auto_increment primary key,
     nome varchar(30) not null,
     dia1 DATETIME DEFAULT NOW(),
 	dia2 DATETIME DEFAULT NOW(),
-    primary key(id)
+	dia3 DATETIME DEFAULT NOW(),
+	dia4 DATETIME DEFAULT NOW(),
+	dia5 DATETIME DEFAULT NOW()
 );
 
-ALTER TABLE person ADD dia3 DATETIME DEFAULT NOW();
-ALTER TABLE person ADD dia4 DATETIME DEFAULT NOW();
-ALTER TABLE person ADD dia5 DATETIME DEFAULT NOW();
-
+--DML:
 insert into person (nome) values ("dan"), ("den"), ("din"), ("don"), ("dun");
