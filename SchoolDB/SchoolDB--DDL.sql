54202e75ca665a3d16dffa4fb9f9e92befd3c40f
bispo-daniel <dbispo.work@gmail.com>
2023-04-12 16:24:21 -0300

    updateSchoolDB

--- SchoolDB/SchoolDB--DDL.sql
+++ SchoolDB/SchoolDB--DDL.sql
@@ -2,80 +2,55 @@ CREATE DATABASE IF NOT EXISTS SchoolDB;
 USE SchoolDB;
 
 CREATE TABLE IF NOT EXISTS turma ( 
- idturma BIGINT PRIMARY KEY AUTO_INCREMENT,  
+ id_turma INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,  
  nome VARCHAR(100) NOT NULL,  
  periodo CHAR(1) NOT NULL  
-); 
-
-CREATE TABLE IF NOT EXISTS responsavel_financeiro ( 
- idresponsavel_financeiro BIGINT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(100) NOT NULL,  
- cpf BIGINT NOT NULL,  
- nascimento DATE NOT NULL,  
- endereco VARCHAR(100) NOT NULL,  
- telefone BIGINT NOT NULL,  
- email VARCHAR(100) NOT NULL  
-); 
-
-CREATE TABLE IF NOT EXISTS responsavel_pedagogico ( 
- idresponsavel_pedagogico BIGINT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(100) NOT NULL,  
- cpf BIGINT NOT NULL,  
- nascimento DATE NOT NULL,  
- endereco VARCHAR(100) NOT NULL,  
- telefone BIGINT NOT NULL,  
- email VARCHAR(100) NOT NULL  
-); 
+);
 
 CREATE TABLE IF NOT EXISTS aluno ( 
- ra BIGINT PRIMARY KEY AUTO_INCREMENT,  
+ RA INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,  
  nome VARCHAR(100) NOT NULL,  
- cpf BIGINT NOT NULL,  
+ cpf INT NOT NULL,  
  nascimento DATE NOT NULL,  
  endereco VARCHAR(100) NOT NULL,  
- telefone BIGINT NOT NULL,  
+ telefone INT NOT NULL,  
  email VARCHAR(100) NOT NULL,  
- idresponsavel_pedagogico BIGINT NOT NULL,  
- idresponsavel_financeiro BIGINT NOT NULL,  
- idturma BIGINT NOT NULL  
-); 
-
-CREATE TABLE IF NOT EXISTS nota ( 
- idnota BIGINT PRIMARY KEY AUTO_INCREMENT,  
- nota BIGINT NOT NULL,  
- idmateria BIGINT NOT NULL,  
- ra BIGINT NOT NULL  
-); 
-
-CREATE TABLE IF NOT EXISTS professor ( 
- idprofessor BIGINT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(100) NOT NULL,  
- cpf BIGINT NOT NULL,  
- valor_hora_aula BIGINT NOT NULL,  
- nascimento DATE NOT NULL,  
- endereco VARCHAR(100) NOT NULL,  
- telefone BIGINT NOT NULL,  
- email VARCHAR(100) NOT NULL  
-); 
+ fk_id_turma INT UNSIGNED NOT NULL,
+ FOREIGN KEY (fk_id_turma) REFERENCES turma(id_turma)
+);
 
 CREATE TABLE IF NOT EXISTS materia ( 
- idmateria BIGINT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(100) NOT NULL  
+ id_materia INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(100) NOT NULL
+ ); 
+
+CREATE TABLE IF NOT EXISTS nota ( 
+ id_nota INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,  
+ nota INT UNSIGNED NOT NULL,  
+ fk_id_materia INT UNSIGNED NOT NULL,  
+ fk_RA_aluno INT UNSIGNED NOT NULL,
+ FOREIGN KEY (fk_id_materia) REFERENCES materia (id_materia),
+ FOREIGN KEY (fk_RA_aluno) REFERENCES aluno (RA)
+);
+
+CREATE TABLE IF NOT EXISTS professor ( 
+ id_professor INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(100) NOT NULL,  
+ cpf INT NOT NULL,  
+ valor_hora_aula INT NOT NULL,  
+ nascimento DATE NOT NULL,  
+ endereco VARCHAR(100) NOT NULL,  
+ telefone INT NOT NULL,  
+ email VARCHAR(100) NOT NULL  
 ); 
 
 CREATE TABLE IF NOT EXISTS materia_professor (  
- idturma BIGINT NOT NULL,  
- idprofessor BIGINT NOT NULL,  
- idmateria BIGINT NOT NULL,
- CONSTRAINT idmateria_professor PRIMARY KEY (idturma, idprofessor, idmateria)
+ id_turma INT UNSIGNED NOT NULL,  
+ id_professor INT UNSIGNED NOT NULL,  
+ id_materia INT UNSIGNED NOT NULL,
+ CONSTRAINT id_materia_professor PRIMARY KEY (id_turma, id_professor, id_materia)
 ); 
 
-ALTER TABLE aluno ADD FOREIGN KEY (idresponsavel_pedagogico) REFERENCES responsavel_pedagogico (idresponsavel_pedagogico);
-ALTER TABLE aluno ADD FOREIGN KEY (idresponsavel_financeiro) REFERENCES responsavel_financeiro (idresponsavel_financeiro);
-ALTER TABLE aluno ADD FOREIGN KEY (idturma) REFERENCES turma (idturma);
-ALTER TABLE nota ADD FOREIGN KEY (idmateria) REFERENCES materia (idmateria);
-ALTER TABLE nota ADD FOREIGN KEY (ra) REFERENCES aluno (ra);
-
-ALTER TABLE materia_professor ADD FOREIGN KEY (idturma) REFERENCES turma (idturma);
-ALTER TABLE materia_professor ADD FOREIGN KEY (idprofessor) REFERENCES professor (idprofessor);
-ALTER TABLE materia_professor ADD FOREIGN KEY (idmateria) REFERENCES materia (idmateria);
+ALTER TABLE materia_professor ADD FOREIGN KEY (id_turma) REFERENCES turma (id_turma);
+ALTER TABLE materia_professor ADD FOREIGN KEY (id_professor) REFERENCES professor (id_professor);
+ALTER TABLE materia_professor ADD FOREIGN KEY (id_materia) REFERENCES materia (id_materia);
