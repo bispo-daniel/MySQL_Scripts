34af611af4320e38210668a080b6931f26470967
bispo-daniel <dbispo.work@gmail.com>
2023-04-25 15:54:32 -0300

    Update RandomDataDB--DDL.sql

--- RandomDataDB/RandomDataDB--DDL.sql
+++ RandomDataDB/RandomDataDB--DDL.sql
@@ -1,18 +1,6 @@
 CREATE DATABASE IF NOT EXISTS randomdatadb;
 use randomdatadb;
 
-select * from randomClient;
-
-/*
-10:39:45
-10:40:39
---54s--
-
-10:51:11
-10:52:09
---58s--
-*/
-
 CREATE TABLE IF NOT EXISTS randomClient (
   id bigint auto_increment primary key,
   randomName varchar(100) not null,
@@ -22,6 +10,14 @@ CREATE TABLE IF NOT EXISTS randomClient (
   insertDateTime datetime DEFAULT NOW()
 );
 
-insert into randomClient (randomName, randomBalance, randomDate, randomBank) 
-values ('a', 1, '1970-1-6', 'OKOK');
+select * from randomClient;
 
+select sum(randomBalance) from randomClient;
+
+select id, randomBalance from randomClient ORDER BY randomBalance;
+
+/*
+
+drop database randomdatadb;
+
+*/
